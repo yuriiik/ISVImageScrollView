3bc11e2e85f4d25a8583d8be2222babc774a8dfc
Yurii Kupratsevich <yurii@femiglobal.com>
2020-06-17 15:23:44 +0300

    migrate example app to swift

---
+++ Example/ISVImageScrollView/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  ISVImageScrollView_Example
+//
+//  Created by Yurii Kupratsevych on 17.06.2020.
+//  Copyright © 2020 kupratsevich@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+}
+
